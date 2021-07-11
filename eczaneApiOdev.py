0769d9083be92ec6eb11afe91165f430100ba95e
airturker <airtur@hotmail.com>
2021-07-11 12:15:06 +0000

    secimliodeveczaneapi

---
+++ eczaneApiOdev.py
@@ -0,0 +1,35 @@
+import http.client
+import json
+conn=http.client.HTTPSConnection("api.collectapi.com")
+headers={
+    'content-type':"application/json",
+    'authorization':"apikey 1WGj2kLsPQrkKY0woHpy5B:4CcLtI3GdFIiibgIb2zhpl"
+}
+
+il=input("il  giriniz : ")
+if il=="":
+    print("il bilgisi bos, varsayilan olarak Ankara yapildi")
+    il=="Ankara"
+else:
+    il=il.strip().capitalize()
+
+ilce=input("ilce giriniz : ")
+if ilce=="":
+    print("ilce bilgisi bos, varsayilan olarak Cankaya yapildi")
+    ilce=="Cankaya"
+else:
+    il=il.strip().capitalize()
+print(il,ilce)
+
+bilgi="/health/dutyPharmacy?ilce="+ilce+"&il"+il
+conn.request("GET",bilgi,headers=headers)
+res=conn.getresponse()
+data=res.read()
+veri=data.decode("utf-8")
+json_veri=json.loads(veri)
+
+if json_veri["success"]==True:
+    bilgi=json_veri["result"][0]
+    print("eczane adi : "+ bilgi["name"]+"adresi : "+bilgi["address"])
+else:
+    print("istek basarisiz")
